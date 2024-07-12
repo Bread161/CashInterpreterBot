601238220fa6a1cc5806a607f8568b4fc26871e5
Anton <toxa-thebest@mail.ru>
2024-07-12 11:28:16 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = "7441566469:AAE5WQUmEw58_gquG0MFy0JvZYGs8Wyxkps"
+
+keys = {
+    'доллар':'USD',
+    'евро':'EUR',
+    'юань':'CNY',
+    'биткоин':'BTC',
+    'эфириум':'ETH',
+    'USD Coin':'USDC',
+    'Notcoin':'NOT'
+}
